d89e3044383975642ff22fdf730546f6eb9b7a44
Roberto Bochet <r@robertobochet.me>
2026-07-19 03:17:32 +0200

    fix(pre-commit): migrate commitlint config to cjs

---
+++ commitlint.config.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  "extends": [
+    "@commitlint/config-conventional"
+  ],
+  "plugins": [
+    "commitlint-plugin-function-rules"
+  ],
+  "rules": {
+    "type-enum": [
+      2,
+      "always",
+      ["chore", "feat", "fix", "docs", "ci", "refactor"]
+    ],
+    "scope-enum": [0],
+    "function-rules/header-max-length": [
+      2,
+      "always",
+      ({type, scope}) => {
+        const selectiveScopes = {
+          feat: ["k0s", "almalinux", "packages", null],
+          fix: ["k0s", "pre-commit", "renovate", null],
+          docs: ["readme", null],
+          ci: [null],
+          chore: ["almalinux", "pre-commit", "renovate", null],
+          refactor: [null]
+        }
+
+        if(!selectiveScopes[type]) return [true]
+        if(!scope) return [true]
+
+        if (selectiveScopes[type].includes(scope)) return [true]
+
+        return [
+          false,
+          scope
+            ? `Scope "${scope}" is not permitted for type "${type}"`
+            : `A scope is required for type "${type}"`
+        ]
+      }
+    ]
+  }
+}
